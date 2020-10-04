dd50f3e3c551fa675050fe64597900910052bfee
Solomon <solomonmeresa8@gmail.com>
2020-10-03 21:02:17 -0700

    adds delete request and deletes item from UI

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -7,19 +7,18 @@ const {
 } = require("../../lib/notes");
 const { notes } = require("../../data/notes");
 
-// ------------- ROUTES ------------------------------------
+// ==================   GET =========================================
 
 router.get("/notes", (req, res) => {
   let results = notes;
-  //   if (req.query) {
-  //     results = filterByQuery(req.query, results);
-  //   }
+
   res.json(results);
 });
 
+// ==================   POST =========================================
+
 router.post("/notes", (req, res) => {
-  // set id based on what the next index of the array will be
-  //   req.body.id = notes.length.toString();
+  // assign unique id
   req.body.id = uniqid();
 
   // if any data in req.body is incorrect, send 400 error back
@@ -31,10 +30,9 @@ router.post("/notes", (req, res) => {
   }
 });
 
-router.delete("/notes/:id", (req, res) => {
-  console.log(req.params.id);
-  // if any data in req.body is incorrect, send 400 error back
+// ==================   DELETE =========================================
 
+router.delete("/notes/:id", (req, res) => {
   const note = deleteNoteById(req.params.id, notes);
   res.json(note);
 });
