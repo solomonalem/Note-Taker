1a7aa4a151ffce191a44f4aa37584b0ad2b2c69d
Solomon <solomonmeresa8@gmail.com>
2020-10-03 20:52:55 -0700

    adds delete request and deletes item from UI

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,34 +1,26 @@
 const router = require("express").Router();
+const uniqid = require("uniqid");
 const {
-  filterByQuery,
-  findById,
+  deleteNoteById,
   createNewNote,
   validateNote,
 } = require("../../lib/notes");
 const { notes } = require("../../data/notes");
 
-// ------------- ROUTES -------------------
+// ------------- ROUTES ------------------------------------
 
 router.get("/notes", (req, res) => {
   let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
+  //   if (req.query) {
+  //     results = filterByQuery(req.query, results);
+  //   }
   res.json(results);
-});
-
-router.get("/notes/:id", (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
 });
 
 router.post("/notes", (req, res) => {
   // set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
+  //   req.body.id = notes.length.toString();
+  req.body.id = uniqid();
 
   // if any data in req.body is incorrect, send 400 error back
   if (!validateNote(req.body)) {
@@ -37,6 +29,14 @@ router.post("/notes", (req, res) => {
     const note = createNewNote(req.body, notes);
     res.json(note);
   }
+});
+
+router.delete("/notes/:id", (req, res) => {
+  console.log(req.params.id);
+  // if any data in req.body is incorrect, send 400 error back
+
+  const note = deleteNoteById(req.params.id, notes);
+  res.json(note);
 });
 
 module.exports = router;
